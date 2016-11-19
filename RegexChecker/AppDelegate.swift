330019f1f122cb86ca4a1507a658982a23be16c7
gfoster <gary@taskrabbit.com>
2016-11-18 16:09:54 -0800

    initial commit from local git

---
+++ RegexChecker/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  RegexChecker
+//
+//  Created by Gary Foster on 11/18/16.
+//  Copyright © 2016 Gary Foster. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ RegexChecker/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  RegexChecker
+//
+//  Created by Gary Foster on 11/18/16.
+//  Copyright © 2016 Gary Foster. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
